64e4d223c9469d6166913a633c210ac2ef7871a6
Frank Condezo <frankcondezo@gmail.com>
2024-04-18 19:07:49 -0500

    update things

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,31 +1,30 @@
 ﻿using System.Diagnostics;
 using ConsoleApp1;
+using Microsoft.EntityFrameworkCore;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // // Start the stopwatch
-        Stopwatch stopwatch = Stopwatch.StartNew();
-        //
-        // // Your Entity Framework code to establish the connection
-        // using (var dbContext = new MyDbContext())
-        // {
-        //     // This will trigger the actual connection to the database
-        //     dbContext.Database.CanConnect();
-        // }
-        //
-        // // Stop the stopwatch
         using (var spannerSampleDbContext = new BloggingContext())
         {
-            // This will trigger the actual connection to the database
+            long total = 0;
+            long iterations = 100;
+            var timer = Stopwatch.StartNew();
+            
             spannerSampleDbContext.Database.CanConnect();
+            
+            for (int i = 0; i < iterations; i++)
+            {
+                
+                timer.Start();
+                spannerSampleDbContext.Blogs.FromSqlRaw("SELECT 1");
+                timer.Stop();
+                total = total + timer.ElapsedMilliseconds;
+            }
+        
+            Console.WriteLine($"Avg time: {((double)total/iterations)} in milliseconds");
         }
         
-        stopwatch.Stop();
-        //
-        // // Output the elapsed time
-        Console.WriteLine($"Time taken to establish connection: {stopwatch.ElapsedMilliseconds} milliseconds");
-
     }
 }
